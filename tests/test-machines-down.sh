d5cf55355f26c99b401c47812937c1b41cad18a7
Tyler Darnell <tyler.a.darnell@gmail.com>
2021-02-22 08:51:06 -0600

    properly remove domains

--- tests/test-machines-down.sh
+++ tests/test-machines-down.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-virsh destroy test1
-virsh destroy test2
-virsh undefine test1
-virsh undefine test2
+sudo virsh destroy test1
+sudo virsh destroy test2
+sudo virsh undefine test1
+sudo virsh undefine test2
 
 rm -f test1.qcow2
 rm -f test2.qcow2
